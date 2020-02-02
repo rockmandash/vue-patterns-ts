4f0baa7dc78bc3088a90c315082420d33eea2e3e
Joseph Wang <jamesxxxxxxx@gmail.com>
2020-02-02 21:26:46 +0800

    update

--- example/components/ExampleContainer.vue
+++ example/components/ExampleContainer.vue
@@ -11,14 +11,39 @@
         :exampleName="exampleName"
       />
     </section>
+
+    <h2 class="nes-text is-primary">Options</h2>
+
+    <div class="nes-table-responsive">
+      <table class="nes-table is-bordered is-dark">
+        <tbody>
+          <tr v-for="(value, name) in utilOptions" :key="name">
+            <td class="nes-text is-primary">{{ name }}</td>
+            <td class="nes-text is-success">
+              {{ value ? value.toString() : value }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import flat from 'flat';
 import Example from './Example.vue';
+import { exportAllUtilNameOptions } from '@/components/auto-generated/meta';
 
 export default Vue.extend({
+  data() {
+    return {
+      utilOptions: flat(
+        // @ts-ignore
+        exportAllUtilNameOptions[`${this.$route.params.utilName}Options`]
+      )
+    };
+  },
   props: {
     examplesData: Object
   },
@@ -35,5 +60,13 @@ export default Vue.extend({
   &:first-child {
     margin-top: 0;
   }
+}
+
+h2.nes-text.is-primary {
+  margin-top: 2rem;
+}
+
+.nes-table-responsive {
+  position: relative;
 }
 </style>
