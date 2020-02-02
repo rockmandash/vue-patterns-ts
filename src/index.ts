1c18385550d606b603e9bafbbfc4cfb16b7bb25f
Joseph Wang <jamesxxxxxxx@gmail.com>
2020-02-02 21:03:04 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
-export { WindowEvent } from './main/browser/WindowEvent';
-export { Log } from './main/development/Log';
+export { WindowEvent, WindowEventOptions } from './main/browser/WindowEvent';
+export { Log, LogOptions } from './main/development/Log';
 export {
   WindowSize,
+  WindowSizeOptions,
   WindowSizeEmittedDataType
 } from './main/sensors/WindowSize';
 export {
   MousePositionAtPage,
+  MousePositionAtPageOptions,
   MousePositionAtPageEmittedDataType
 } from './main/sensors/MousePositionAtPage';
 export {
   WindowScroll,
+  WindowScrollOptions,
   WindowScrollEmittedDataType
 } from './main/sensors/WindowScroll';
-export { Interval, IntervalEmittedDataType } from './main/animations/Interval';
-export { Toggle } from './main/state/Toggle';
+export {
+  Interval,
+  IntervalOptions,
+  IntervalEmittedDataType
+} from './main/animations/Interval';
+export { Toggle, ToggleOptions } from './main/state/Toggle';
