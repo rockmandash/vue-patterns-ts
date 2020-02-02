beb47c86cd4a8aa257147ab826c333be12ae5cfe
Joseph Wang <jamesxxxxxxx@gmail.com>
2020-02-02 21:38:29 +0800

    Update index.vue

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    Home
+    <h1 class="nes-text is-primary">vue-patterns-ts</h1>
+    <p>Collection of best practice vue utilities written in TypeScript</p>
+    <p>
+      This example website is built up on Nuxt.js, so all the utilities is
+      compatibal with SSR
+      <i class="nes-icon is-large heart"></i>
+    </p>
+
+    <h1 class="nes-text is-primary">Installation</h1>
+
+    <highlight-code lang="html">
+      yarn add vue-patterns-ts
+    </highlight-code>
+
+    <highlight-code lang="html">
+      npm install vue-patterns-ts
+    </highlight-code>
   </div>
 </template>
 
